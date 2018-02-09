fb86956bb38bb02a86c2760b1e0aa6f0b38de300
rwart <32432397+rwart@users.noreply.github.com>
2018-02-09 14:37:49 +0100

    ready for review

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 
 var express = require('express');
 var app = express();
-app.use(express.static('assets')); // index.html is served from ./assets
 
 /*
-app.get('/', function (req, res) { // index.html can be served from ./
-  res.sendFile('/index.html');
+app.get('/', function (req, res) { // index.html can be served from ./ as default entry
+  res.sendFile('/index.html', { root: __dirname });
+});
+
+app.get('/index.html', function (req, res) { // index.html can be served from ./
+  res.sendFile('/index.html', { root: __dirname });
 });
 */
+
+app.use(express.static('assets')); // index.html and default entry can served from ./assets
 
 app.get('/userform', function (req, res) {
   const response = {
@@ -16,6 +21,14 @@ app.get('/userform', function (req, res) {
   };
   res.json(response);
   console.log('req useform url: ' + req.originalUrl);
+});
+
+app.use(function (req, res, next) {
+  var msg = 'request endpoint not supported: method: ' +
+   req.method + ' url: ' + req.originalUrl;
+
+  res.status(404).send(msg);
+  console.log(msg);
 });
 
 var server = app.listen(3000, 'localhost', function () {
